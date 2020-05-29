7a4dea8fff3cc86c5c463b147fcb4733460e6aad
JoseLRiano <joselriano@yahoo.com>
2020-05-29 16:29:40 -0400

    Updates

---
+++ precache-manifest.2a42ce304c57a7be25ead9d152678034.js
@@ -0,0 +1,26 @@
+self.__precacheManifest = (self.__precacheManifest || []).concat([
+  {
+    "revision": "f268a9f19a3f85413c238c4774ff946c",
+    "url": "/DeckOfCardsReact/index.html"
+  },
+  {
+    "revision": "adfac3ec0686cf5ec02e",
+    "url": "/DeckOfCardsReact/static/css/main.1f47c42f.chunk.css"
+  },
+  {
+    "revision": "6d0c4d771c612b6b9889",
+    "url": "/DeckOfCardsReact/static/js/2.ca3d19ce.chunk.js"
+  },
+  {
+    "revision": "e88a3e95b5364d46e95b35ae8c0dc27d",
+    "url": "/DeckOfCardsReact/static/js/2.ca3d19ce.chunk.js.LICENSE.txt"
+  },
+  {
+    "revision": "adfac3ec0686cf5ec02e",
+    "url": "/DeckOfCardsReact/static/js/main.10b9bfda.chunk.js"
+  },
+  {
+    "revision": "943a58d2ffb0a68f27ef",
+    "url": "/DeckOfCardsReact/static/js/runtime-main.a544c342.js"
+  }
+]);
